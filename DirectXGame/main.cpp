972301f81f48806c43638a77f2139d878676a07c
上野太己 <k023g0051@g.neec.ac.jp>
2025-06-18 11:38:21 +0900

    00_08_確認課題

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -50,14 +50,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// リソースの確保含め、頂点情報を柔軟に対応できるようにVertexData構造体を新たに作成する
 	struct VertexDate {
 		Vector4 position;
+		Vector2 texcoord;
 	};
 
 	// 頂点データの準備
 	VertexDate vertices[] = {
-		{ -1.0f, -1.0f, 0.0f, 1.0f }, // 左下
-		{ -1.0f, 1.0f, 0.0f, 1.0f }, // 左上
-		{ 1.0f, -1.0f, 0.0f, 1.0f }, // 右下
-		{ 1.0f, 1.0f, 0.0f, 1.0f }, // 右上
+		{ { -1.0f, -1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f } }, // 左下
+		{ { -1.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 0.0f } }, // 左上
+		{ { 1.0f, -1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } }, // 右下
+		{ { 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 0.0f } }, // 右上
 	};
 
 	// VertexBufferの生成
@@ -123,11 +124,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 // 引数として空のpipelineState、RootSinature、頂点シェーダーvs、ピクセルシェーダーpsを参照で受け取る
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) { 
 	// InputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
