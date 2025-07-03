b1d3aaa52a8a9bba5741c3e7db8e6bd98fa3c347
上野太己 <k023g0051@g.neec.ac.jp>
2025-07-03 10:12:54 +0900

    00_09_確認課題

--- DirectXGame/RootSignature.cpp
+++ DirectXGame/RootSignature.cpp
@@ -20,15 +20,6 @@ void RootSignature::Create() {
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
 	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
-	ID3DBlob* signatureBlob = nullptr;
-	ID3DBlob* errorBlog = nullptr;
-	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature,
-		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
-		assert(false);
-	}
-
 	// デスクリプタレンジ
 	D3D12_DESCRIPTOR_RANGE srvDescRange[1]{};
 	// t0 レジスタを利用可能にする
@@ -61,6 +52,15 @@ void RootSignature::Create() {
 
 	descriptionRootSignature.pStaticSamplers = staticSamplers;
 	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
+
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlog = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+		D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+		assert(false);
+	}
 
 	// バイナリをもとに生成
 	ID3D12RootSignature* rootSignature = nullptr;
