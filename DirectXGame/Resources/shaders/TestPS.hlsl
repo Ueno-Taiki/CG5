d663afb82d99056c6c35b906c2990c166679c4b1
上野太己 <k023g0051@g.neec.ac.jp>
2025-07-09 10:49:40 +0900

    00_10_確認課題

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -9,8 +9,13 @@ struct PixelShaderOutput {
 
 PixelShaderOutput main(VertexShaderOutput input) {
     PixelShaderOutput output;
+
     float32_t2 uv = input.texcoord;
     float32_t4 textureColor = gTexture.Sample(gSampler, uv);
-    output.color = textureColor;  // non effect
+    
+    // grayscale
+    float32_t value = dot(textureColor.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    output.color = float32_t4(value, value, value, textureColor.a);
+
     return output; 
 };
